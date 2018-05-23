e3050942197616809066f8364657f18e5240ea47
hyurl <i@hyurl.com>
2018-05-23 13:35:13 +0800

    add travis tests

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
-const StringTrimmer = require("./");
+const trimmer = require("./");
 const assert = require("assert");
 
-assert(StringTrimmer.trim("  test  ") === "test");
-assert(StringTrimmer.trimLeft("  test  ") === "test  ");
-assert(StringTrimmer.trimRight("  test  ") === "  test");
+assert.equal(trimmer.trim("  test  "), "test");
+assert.equal(trimmer.trimLeft("  test  "), "test  ");
+assert.equal(trimmer.trimRight("  test  "), "  test");
 
-assert(StringTrimmer.trim("test", "t") === "es");
-assert(StringTrimmer.trim("testing", "ing") === "test");
-assert(StringTrimmer.trimLeft("test", "t") === "est");
-assert(StringTrimmer.trimRight("test", "t") === "tes");
+assert.equal(trimmer.trim("test", "t"), "es");
+assert.equal(trimmer.trim("testing", "ing"), "test");
+assert.equal(trimmer.trimLeft("test", "t"), "est");
+assert.equal(trimmer.trimRight("test", "t"), "tes");
 
 // Optionally, you could apply these methods to String.prototype.
-StringTrimmer.applyTo(String.prototype);
+trimmer.applyTo(String.prototype);
 
-assert(" test ".trim(" t") === "es");
+assert.equal(" test ".trim(" t"), "es");
 
-console.log("pass");
+console.log("#### OK ####");
